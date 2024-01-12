2f5b9979ab59994bd79bd75e469593ab28851844
emin123213 <153289947+emin123213@users.noreply.github.com>
2024-01-12 14:54:37 +0400

    Add files via upload

---
+++ pokemon.cy.js
@@ -0,0 +1,127 @@
+describe('Покупка аватара', function () {
+   it('e2e тест на покупку нового аватара для тренера', function () {
+        cy.visit('https://pokemonbattle.me/');
+        cy.get(':nth-child(1) > .auth__input').type('e.aliasgarov2@mail.ru');
+        cy.get('#password').type('Emin2001');
+        cy.get('.auth__button').click();
+        cy.get('.header__btns > [href="/shop"]').click();
+        cy.get(':nth-child(1) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(2) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(3) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(4) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(5) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(6) > .shop__button').click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(7) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(8) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.wait(1000);
+        cy.get(':nth-child(9) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(10) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(11) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+        cy.get(':nth-child(12) > .shop__button').click({ force: true });
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('German Dolnikov');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+
+
+
+
+
+
+
+
+
+          })
+})
